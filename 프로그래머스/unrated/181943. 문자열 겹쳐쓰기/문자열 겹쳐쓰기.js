ac92dd4713d59405c460fa5ffe817360cb67b7b2
devDobby <porphyria95@gmail.com>
2023-06-08 16:42:29 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,6 @@
+const solution = (my_string, overwrite_string, s) => {
+    let answer = [...my_string];
+    answer.splice(s, overwrite_string.length, overwrite_string);
+    return answer.join('');
+}
+    
