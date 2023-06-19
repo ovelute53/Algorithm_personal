b15ff213e5d164f9e95444f65b4cbdd0b8013783
devDobby <porphyria95@gmail.com>
2023-06-19 16:26:24 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 6.31 ms, Memory: 45.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,13 @@
+const solution = (arr) => {
+    let answer = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] >= 50 && arr[i] % 2 === 0){
+            answer.push(arr[i] / 2)
+        } else if(arr[i] < 50 && arr[i] % 2 === 1) {
+            answer.push(arr[i] * 2)
+        } else {
+            answer.push(arr[i])
+        }
+    }
+    return answer;
+}
