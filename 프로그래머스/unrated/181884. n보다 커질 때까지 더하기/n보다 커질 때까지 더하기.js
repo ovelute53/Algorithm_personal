de863c1a5254de525afe47669203c5ce0542fb82
devDobby <porphyria95@gmail.com>
2023-06-22 09:49:58 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,10 @@
+const solution = (numbers, n) => {
+    let answer = 0;
+    for(let i = 0; i < numbers.length; i++) {
+        if(answer <= n){
+             answer += numbers[i]
+        }
+    }
+    
+    return answer;
+}
