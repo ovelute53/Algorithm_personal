e318b5c0ff6d7d007820252b9f5a44653a484049
devDobby <porphyria95@gmail.com>
2023-06-22 17:11:06 +0900

    [unrated] Title: 수 조작하기 1, Time: 5.51 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,6 @@
+const solution = (n, control) => {
+    for(let i = 0; i < control.length; i++) {
+        if(control[i] === "w") {n += 1} else if(control[i] === "s") {n -= 1} else if(control[i] === "d") {n += 10} else if(control[i] === "a") {n -= 10};
+    }
+    return n;
+}
